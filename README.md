027b94365ff94c8270a9d338f96363afc9acdf2f
Lewis Jones <lewismacs20@gmail.com>
2023-11-16 19:14:11 +0000

    code now creates a table in sql

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# firstSQLproject
+# Go PostgreSQL Database Project
+
+This is a simple Go project that demonstrates how to interact with a PostgreSQL database. The project includes functionality to create a product table, insert sample products, and generate random product data.
+
+## Prerequisites
+
+- Go installed on your machine
+- PostgreSQL database server running
+- `github.com/lib/pq` Go package for PostgreSQL
+
+## Getting Started
+
+1. Clone the repository to your local machine:
+
+   ```bash
+   git clone https://github.com/your-username/go-postgresql-project.git
+   ```
+
+- cd go-postgresql-project
+- connectionString := "postgres://your-username:your-password@localhost:5432/your-database?sslmode=disable"
+- go run main.go
+
+Project Structure:
+
+main.go: Contains the main functionality of the project, including database connection, table creation, product insertion, and random data generation.
+
+README.md: This file, providing information about the project.
+
+Functions:
+
+createProductTable(db, \*sql.DB): Creates a 'product' table in the PostgreSQL database.
+
+insertProduct(db sql.DB, product Product) int: Inserts a product into the 'product' table and returns the generated ID.
+
+generateProduct(db, \*sql.DB): Generates and inserts random products into the 'product' table.
+
+generateRandomProduct() string: Generates a random product name from a predefined list.
+
+generatePrice()float64: Generates a random price for a product.
--- main.go
+++ main.go
@@ -1,5 +1,97 @@
 package main
 
-func main(
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"math/rand"
 
+	_ "github.com/lib/pq"
 )
+
+// define a struct for a product.
+type Product struct {
+	Name      string
+	Price     float64
+	Avaliable bool
+}
+
+func main() {
+	connectionString := "postgres://postgres:secret@localhost:5432/postgres?sslmode=disable"
+	// opening a connection to the postgres db.
+	db, err := sql.Open("postgres", connectionString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	// checking the connection to the database.
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	// defer closing the database connection until the main function exits.
+	defer db.Close()
+	// creating the product table in the database
+	createProductTable(db)
+
+	// creating a sample Product and inserting it into the database.
+	product := Product{"Book", 18.99, true}
+	pk := insertProduct(db, product)
+	fmt.Printf("ID = %d\n", pk)
+	// generating and inserting 10 random products into the database.
+	generateProduct(db)
+
+}
+
+// function to generate and insert random products into the database.
+func generateProduct(db *sql.DB) {
+	for i := 0; i < 10; i++ {
+		product := Product{generateRandomProduct(), generatePrice(), true}
+		pk := insertProduct(db, product)
+		fmt.Print(pk)
+	}
+}
+
+// function to generate a random product name from a predefined list.
+func generateRandomProduct() string {
+	productNames := []string{"Book", "Beans", "Milk", "Sugar"}
+
+	// Generate a random number between 0 and 3
+	randomNumber := rand.Intn(4)
+	return productNames[randomNumber]
+
+}
+func generatePrice() float64 {
+	return rand.Float64()
+}
+
+// creating a product table.
+func createProductTable(db *sql.DB) {
+	query := `CREATE TABLE IF NOT EXISTS product(
+		id SERIAL PRIMARY KEY,
+		name VARCHAR(100) NOT NULL,
+		price NUMERIC (6,2) NOT NULL,
+		avaliable BOOLEAN,
+		created timestamp DEFAULT NOW()
+	)`
+
+	// database object to create the table.
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+// function to insert a product into the 'product' table and return the generated ID.
+func insertProduct(db *sql.DB, product Product) int {
+	query := `INSERT INTO product (name, price, avaliable)
+		VALUES ($1,$2,$3) RETURNING id`
+
+	var pk int
+	// executing the query and scanning the generated ID into the 'pk' variable.
+	err := db.QueryRow(query, product.Name, product.Price, product.Avaliable).Scan(&pk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pk
+}
